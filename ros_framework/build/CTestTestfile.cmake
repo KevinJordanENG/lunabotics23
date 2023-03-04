b6618f037d7fcbf2452fef37dfd82a1e9056f6c0
UND Robotics <kevin.jordan.ee@gmail.com>
2023-03-04 15:55:49 -0600

    inital ros catkin workspace created

---
+++ ros_framework/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/und-robotics/lunabotics23/ros_framework/src
+# Build directory: /home/und-robotics/lunabotics23/ros_framework/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ros_framework/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/und-robotics/lunabotics23/ros_framework/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/und-robotics/lunabotics23/ros_framework/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/und-robotics/lunabotics23/ros_framework/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/und-robotics/lunabotics23/ros_framework/src:$ROS_PACKAGE_PATH"
