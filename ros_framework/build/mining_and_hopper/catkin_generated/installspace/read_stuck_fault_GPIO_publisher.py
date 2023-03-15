fd4f4609523d635937b60c265263d6e3449d502d
UND Robotics <kevin.jordan.ee@gmail.com>
2023-03-15 10:50:50 -0500

    commented stuck-fault GPIO ROS node

--- ros_framework/build/mining_and_hopper/catkin_generated/installspace/read_stuck_fault_GPIO_publisher.py
+++ ros_framework/build/mining_and_hopper/catkin_generated/installspace/read_stuck_fault_GPIO_publisher.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
+#import needed libraries
 import rospy
 from std_msgs.msg import Bool
 import Jetson.GPIO as GPIO
 
+#define pin on Jetson (using BOARD pin numbering)
 STUCK_FAULT_GPIO_PIN = 15
 
+#ROS node standard form
 if __name__ == '__main__':
+    #initialize node
     rospy.init_node('read_stuck_fault_GPIO_publisher')
-
+    #setup publishing variable and message for other nodes in ROS framework
     pub = rospy.Publisher('stuck_fault_state', Bool, queue_size = 10)
-
+    #setup pin for GPIO
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(STUCK_FAULT_GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-    rate = rospy.Rate(10)
-
+    #set message publishing rate (Hz)
+    rate = rospy.Rate(50)
+    #keep node running unless CTRL C pressed
     while not rospy.is_shutdown():
+        #read GPIO pin for stuck-fault signal from arduino
         stuck_fault = GPIO.input(STUCK_FAULT_GPIO_PIN)
+        #publish message to ROS framework
         pub.publish(stuck_fault)
+        #wait for specified publish rate
         rate.sleep()
-
+    #cleanup GPIO pin
     GPIO.cleanup()
