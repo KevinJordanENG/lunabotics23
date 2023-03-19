0f2a0d38b4d495c8726f724e93123214a0184020
UND Robotics <kevin.jordan.ee@gmail.com>
2023-03-19 13:59:20 -0500

    porting CTRE API to ROS motor node

--- ros_framework/src/mining_and_hopper/src/hopper_motor_subscriber.cpp
+++ ros_framework/src/mining_and_hopper/src/hopper_motor_subscriber.cpp
@@ -1,6 +1,20 @@
+//Phoenix & Talon SRX includes
+#define Phoenix_No_WPI
+#include "mining_and_hopper/Phoenix.h"
+#include "mining_and_hopper/phoenix/platform/Platform.h"
+#include "mining_and_hopper/phoenix/unmanaged/Unmanaged.h"
+#include "mining_and_hopper/phoenix/cci/Unmanaged_CCI.h"
+
+//ROS includes
 #include "ros/ros.h"
 #include "std_msgs/Int16.h"
+
+//sys includes
+#include <string>
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <unistd.h>
 
 class command_data {
 public:
@@ -25,8 +39,18 @@ int main(int argc, char **argv){
     while (ros::ok()) {
         ros::spinOnce();
         loop_rate.sleep();
-        std::cout << input_sig.hopper_motor_input_cmd;
-        std::cout << "\n";
+        if (input_sig.hopper_motor_input_cmd == 0) {
+            std::cout << "OFF\n";
+        }
+        else if (input_sig.hopper_motor_input_cmd == 1) {
+            std::cout << "FWD\n";
+        }
+        else if (input_sig.hopper_motor_input_cmd == -1) {
+            std::cout << "REV\n";
+        }
+        else {
+            std::cout << "OFF\n";
+        }
     }
 
     return 0;
